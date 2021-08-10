904e230f1ac2b59e6a440579009ddec57203ced7
MKing_abc_com <mking@kingproc.com>
2021-08-10 12:04:45 -0500

    patient controller edits

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PatientContoller extends Controller
+class PatientController extends Controller
 {
     /**
      * Display a listing of the resource.
