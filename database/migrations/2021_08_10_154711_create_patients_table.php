ac951ec88516d0742dd313213d0d39c43ad65f36
MKing_abc_com <mking@kingproc.com>
2021-08-10 12:46:49 -0500

    DOB fix

--- database/migrations/2021_08_10_154711_create_patients_table.php
+++ database/migrations/2021_08_10_154711_create_patients_table.php
@@ -21,9 +21,7 @@ class CreatePatientsTable extends Migration
             $table->string('name_first');
             $table->string('name_middle')->nullable();
             $table->string('name_last')->nullable();
-            $table->integer('dob_month')->nullable();
-            $table->integer('dob_day')->nullable();
-            $table->integer('dob_year')->nullable();
+            $table->date('dob')->nullable();
             $table->string('sex')->nullable();
             $table->string('ethnicity')->nullable();
             $table->integer('primary_contact_id')->nullable();
